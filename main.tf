6f5ff7d4a5db706912511f1594d27a1c9f2027ed
darshansb1105 <darshan.bhagwat1105@gmail.com>
2023-10-25 17:01:13 +0530

    resource group updated

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_template_deployment" "example" {
-  name                = "example-deployment"
-  resource_group_name = "example-resources"
+resource "azurerm_resource_group_template_deployment" "example" {
+  name                = "centroEdge-deployment"
+  resource_group_name = "rg-centro-admin-spoke-test-001"
   deployment_mode     = "Incremental"
 
-  template_body = file("${path.module}/template.json")
+  template_content =  file("${path.module}/template.json")
   parameters_content = file("${path.module}/parameters.json")
 }
